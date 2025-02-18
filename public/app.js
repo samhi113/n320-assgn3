5b535364bf14b859406b667d784f30d5b425a169
samhigg <samhigg@iu.edu>
2025-02-17 22:47:14 -0500

    finally done :P

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ var count = 0;
 
 theButton.addEventListener("click", function() {
     count += 1;
-    domManSec.innerHTML = `<p>You did the thing where the thing is did ${count} times!</p>
+    domManSec.innerHTML = `<p>You did the thing where the thing is did ${count} time(s)!</p>
     <p>Isn't that cool? When the user clicks the button, a function is called to manipulate the DOM and add text locally.</p>
     <p>Now check this out. <button id="betterButton">Click me too!</button></p>`;
 
@@ -15,6 +15,6 @@ theButton.addEventListener("click", function() {
     betterButton.addEventListener("click", function() {
         var img = document.createElement("img");
         img.setAttribute("src", "./img/wave.gif");
-        domManSec.append(img)
+        domManSec.append(img);
     })
 });
--- server.js
+++ server.js
@@ -3,16 +3,65 @@ const fs = require("fs");
 const path = require("path");
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end("<h1>404 Not Found</h1>");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(content);
-    }
-  });
+  if (req.url === "/" || req.url === "/index.html") {
+    const htmlFilePath = path.join(__dirname, "public/index.html");
+    fs.readFile(htmlFilePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 - HTML Not Found</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    });
+  } else if (req.url === "/about.html") {
+    const htmlFilePath = path.join(__dirname, "public/about.html");
+    fs.readFile(htmlFilePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 - HTML Not Found</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    });
+  } else if (req.url === "/app.js") {
+    const jsFilePath = path.join(__dirname, "public/app.js");
+    fs.readFile(jsFilePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/js" });
+        res.end("<h1>404 - app.js Not Found</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/js" });
+        res.end(content);
+      }
+    });
+  } else if (req.url === "/styles.css") {
+    const cssFilePath = path.join(__dirname, "public/styles.css");
+    fs.readFile(cssFilePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/css" });
+        res.end("<h1>404 - CSS Not Found</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(content);
+      }
+    });
+  } else if (req.url === "/img/wave.gif") {
+    const imgFilePath = path.join(__dirname, "public/img/wave.gif");
+    fs.readFile(imgFilePath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "image/gif" });
+        res.end("<h1>404 - CSS Not Found</h1>");
+      } else {
+        res.writeHead(200, { "Content-Type": "image/gif" });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 - Not Found</h1>");
+  }
 });
 
 const port = 3000;
